254730c1b22827779e08e09863e302e2548db82f
Sphis-Sinco <sinconsistencia@gmail.com>
2025-03-13 18:17:19 -0500

    attempt at gravity and jumping

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,12 +1,18 @@
 package;
 
+import flixel.FlxG;
 import flixel.FlxSprite;
 import flixel.FlxState;
+import flixel.math.FlxPoint;
 import flixel.util.FlxColor;
 
 class PlayState extends FlxState
 {
 	var player:FlxSprite;
+	var playerSpeedVariables:FlxPoint = new FlxPoint(0, 0);
+
+	final player_gravity_value = 0.1;
+	final player_jumped = FlxG.keys.anyJustReleased([SPACE, Z]);
 
 	override public function create()
 	{
@@ -19,6 +25,15 @@ class PlayState extends FlxState
 
 	override public function update(elapsed:Float)
 	{
+		playerSpeedVariables.y -= player_gravity_value;
+
+		if (player_jumped)
+		{
+			playerSpeedVariables.set(playerSpeedVariables.x, 20);
+		}
+
+		player.y -= playerSpeedVariables.y;
+
 		super.update(elapsed);
 	}
 }
